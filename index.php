a3133e9c7eff5419535351cfb6d245ffb9feeb7a
Khayrul Islam Shanto <77426516+coderkhayrul@users.noreply.github.com>
2022-11-30 11:40:36 +0600

    Opdate Title
    Co-Authored-By: stackmaster <50482101+caliberdevs@users.noreply.github.com>

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>JQUERY CURD</title>
+    <title>JQUERY CURD OPERATION</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <!-- FontAwesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
